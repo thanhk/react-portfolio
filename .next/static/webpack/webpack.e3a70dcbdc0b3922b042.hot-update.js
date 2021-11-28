21e1a5164059a481aff501af8b52f121a913d829
steammbun <stevenkhuu1999@yahoo.com>
2021-11-28 02:18:39 -0600

    add .nojekyll file

--- .next/static/webpack/webpack.e3a70dcbdc0b3922b042.hot-update.js
+++ .next/static/webpack/webpack.e3a70dcbdc0b3922b042.hot-update.js
@@ -12,7 +12,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "aafccc75ff52bbc646c1"; }
+/******/ 	__webpack_require__.h = function() { return "fadc701e1c781229faa9"; }
 /******/ }();
 /******/ 
 /******/ }
