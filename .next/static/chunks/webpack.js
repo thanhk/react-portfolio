66c2597befdacde9b7ef8e032efed24ac395c718
steammbun <stevenkhuu1999@yahoo.com>
2021-11-28 17:31:34 -0600

    favicon

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6a54f12bee38bf0ba46e"; }
+/******/ 		__webpack_require__.h = function() { return "90a6403672a29fcdc6f9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,14 @@
 import Theme from '../styles/theme';
 import Head from 'next/head'
+
+const prefix = '/react-portfolio';
+
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>ThanhK's portfolio</title>
+        <link rel="shortcut icon" href={prefix+'/favicon.ico'}/>
+        <title>My portfolio</title>
       </Head>
       <Theme>
         <Component {...pageProps} />
