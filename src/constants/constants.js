39f0276bfe77c74d191a76b143d151e023f03197
steammbun <stevenkhuu1999@yahoo.com>
2021-11-28 01:57:40 -0600

    prefix image

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -3,7 +3,7 @@ export const projects = [
   {
     title: 'Rise of the Elements',
     description: "An RPG game in Unity Teams using Photon Unity Networking (PUN) for multiplayer functionality. Players had health, mana, experience, inventories, attacks, and sprites that were custom made by us. We also designed an isometric map and implemented mob spawning.",
-      image: '/images/rote1.gif',
+      image: prefix + '/images/rote1.gif',
       tags: ['Unity', 'C#', 'Networking'],
     source: '/',
     visit: '/',
